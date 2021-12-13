7eba698d91a237867f49cf04ba4a0bbcf98a59a2
Santiago Téllez <santiago99tellez@gmail.com>
2021-12-13 15:55:59 -0500

    Tabla inscripciones

--- src/pages/inscripciones/Index.jsx
+++ src/pages/inscripciones/Index.jsx
@@ -1,19 +1,86 @@
-
+import { Table, TableHead, TableCell, TableRow, TableBody, Button} from '@material-ui/core';
 import React, { useEffect } from 'react';
-/*
+import { makeStyles } from '@material-ui/styles';
 import { useMutation, useQuery } from '@apollo/client';
 import PrivateRoute from 'components/PrivateRoute';
 import { toast } from 'react-toastify';
 import LoadingButton from 'components/LoadingButton';
 import { GET_INSCRIPCIONES } from 'graphql/inscripciones/queries';
 import { APROBAR_INSCRIPCION } from 'graphql/inscripciones/mutations';
-*/
+
+const useStyles = makeStyles({
+    table: {
+        align: 'center',
+        borderRadius: '10px',
+        width: '85%',
+        marginLeft: '7.5%',
+        marginTop: '3%'
+    },
+    thead: {
+        '& > *': {
+            width: 'auto',
+            fontSize: 17,
+            background: '#313C46',
+            color: '#FFFFFF',
+            family: 'Quicksand, SemiBold'
+        }
+    },
+    row: {
+        '& > *': {
+            fontSize: 15,
+            color: '#1a497a',
+            family: 'Quicksand,SemiBold'
+        }
+    }
+  })
+
 
 const IndexInscripciones = () => {
+
+    const classes = useStyles();
+
+    const { data, error, loading } = useQuery(GET_INSCRIPCIONES);
+    console.log(data)
+    /*const { ID, estado, proyecto, estudiante, fechaIngreso} = data;*/
+
+    useEffect(() => {
+      if (error) {
+        toast.error('Error consultando inscripciones');
+      }
+    }, [error]);
+  
+    if (loading) return <div>Cargando....</div>;
+
     return (
         <div>
             <div className='H1-header'>Inscripciones</div>
-            <p className='H2-header'>Aquí puedes ver y gestionar los estudiantes inscritos a un proyecto.</p>  
+            <p className='H2-header'>Aquí puedes ver y gestionar los estudiantes inscritos a un proyecto.</p>
+            <Table className = {classes.table}>
+                <TableHead>
+                    <TableRow className = {classes.thead}>
+                        <TableCell>Nombre del proyecto</TableCell>
+                        <TableCell>Nombre del estudiante</TableCell>
+                        <TableCell>Ingreso</TableCell>
+                        <TableCell>Egreso</TableCell>
+                        <TableCell>Estado</TableCell>
+                        <TableCell>Acciones</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        /*useQuery(GET_INSCRIPCIONES).map( data=>(
+                            <TableCell>{data.proyecto.nombre}</TableCell>
+                            <TableCell>{data.estudiante.id}</TableCell>
+                            <TableCell>{data.fechaIngreso}</TableCell>
+                            <TableCell>{data}</TableCell>
+                            <TableCell>{data.estado}</TableCell>
+                            <TableCell>
+                                <Button color="#26ed77" onClick={() => deleteSaleData(sale._id)} >Remove</Button>
+                            </TableCell>
+                        ))*/
+                    }
+                </TableBody>
+            </Table>
         </div>
     )
 }
--- src/pages/usuarios/Index.jsx
+++ src/pages/usuarios/Index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles';
 // ** imports @apollo
 import { useQuery } from '@apollo/client';
 // ** import components
@@ -9,8 +10,37 @@ import PrivateRoute from 'components/PrivateRoute';
 import { GET_USUARIOS } from 'graphql/usuarios/queries';
 // ** import ROLES-enums
 import { Enum_Rol, Enum_EstadoUsuario } from 'utils/enums';
+import { Table, TableHead, TableCell, TableRow, TableBody, Button} from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  table: {
+      align: 'center',
+      borderRadius: '10px',
+      width: '85%',
+      marginLeft: '7.5%',
+      marginTop: '3%'
+  },
+  thead: {
+      '& > *': {
+          width: 'auto',
+          fontSize: 17,
+          background: '#313C46',
+          color: '#FFFFFF',
+          family: 'Quicksand, SemiBold'
+      }
+  },
+  row: {
+      '& > *': {
+          fontSize: 15,
+          color: '#1a497a',
+          family: 'Quicksand,SemiBold'
+      }
+  }
+})
 
 const IndexUsuarios = () => {
+  const classes = useStyles();
   const { data, error, loading } = useQuery(GET_USUARIOS);
 
   useEffect(() => {
@@ -27,26 +57,26 @@ const IndexUsuarios = () => {
       <p className='H2-header'>Aquí puedes ver y asignar estados a los usuarios.</p>  
       <div>
         Datos Usuarios:
-        <table className='tabla'>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Apellidos</th>
-              <th>Correo</th>
-              <th>Identificación</th>
-              <th>Rol</th>
-              <th>Estado</th>
-              <th>Editar</th>
-            </tr>
-          </thead>
-          <tbody>
-          <td>
+        <Table className = {classes.table}>
+          <TableHead >
+            <TableRow className = {classes.thead}>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Apellidos</TableCell>
+              <TableCell>Correo</TableCell>
+              <TableCell>Identificación</TableCell>
+              <TableCell>Rol</TableCell>
+              <TableCell>Estado</TableCell>
+              <TableCell>Editar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          <TableCell>
             <Link to={`/EditarUsuario`}>
               <i className='fas fa-pen text-gray hover:text-orange cursor-pointer' />
             </Link>
-          </td>
-          </tbody>
-        </table>
+          </TableCell>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
