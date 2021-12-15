75c46100a5ca5087d2a52fc2311ae35c8dbe4695
Gina Rodriguez <fixalia@gmail.com>
2021-12-14 21:45:32 -0500

    ajustes import react-router-dom en Login y SignUp

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // ** import @apollo
 import { useMutation } from '@apollo/client';
 // ** import components
