b7c7ff6e22a824dff443835e60d0f107e891fa46
Gina Rodriguez <fixalia@gmail.com>
2021-12-17 23:47:56 -0500

    crear nuevo avance en Index avances y css

--- src/components/LoadingButton.jsx
+++ src/components/LoadingButton.jsx
@@ -7,7 +7,8 @@ const LoadingButton = ({ disabled, loading, text, onClick = () => {} }) => {
       onClick={onClick}
       disabled={disabled}
       type='submit'
-      className='bg-green rounded-3xl shadow-md text-gray-dark font-bold font-Quicksand text-lg p-2.5 text-l rounded-xl hover:bg-gray-light shadow-md my-4 disabled:opacity-50 disabled:bg-gray'
+      className='bg-green rounded-3xl shadow-md text-gray-dark font-bold font-Quicksand
+      text-lg p-2.5 text-l rounded-xl hover:bg-gray-light shadow-md my-4 disabled:opacity-50 disabled:bg-gray'
     >
       {loading ? <ReactLoading type='spin' height={30} width={30} /> : text}
     </button>
--- src/pages/avances/Index.jsx
+++ src/pages/avances/Index.jsx
@@ -16,7 +16,7 @@ import { CREAR_AVANCE } from 'graphql/avances/mutations';
 // ** imports estilos
 import { Dialog } from '@mui/material';
 
-
+/***** Traer el nombre del Proyecto ******/
 const IndexAvance = () => {
     const { projectid } = useParams();
     const [openDialog, setOpenDialog] = useState(false);
@@ -27,12 +27,70 @@ const IndexAvance = () => {
         },
     });
 
+    if (loading) return <div>Loading...</div>;
 
     return (
-        <div className='flex flex-col p-10 items-center w-full'>
-            <h1 className='text-2xl font-bold text-gray-900 my-2'>
-                Avances del proyecto {projectid}
-            </h1>
+        <div>
+            <div className='H1-header'>Avances del proyecto</div>
+            <div className='H2-header'>
+            {projectid}</div>
+            <div className='flew flex-col w-full h-full items-center justify-center p-10 ml-5'>
+            <button
+                onClick={() => setOpenDialog(true)}
+                className='Button-2'
+                type='button'
+            >
+                Crear nuevo avance
+            </button>
+            {/*data.Avances.length === 0 ? (
+                <span>No tienes avances para este proyecto</span>
+            ) : (
+                data.Avances.map((avance) => <IndexAvance avance={avance} />)
+            )*/}
+            <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+                <CrearAvance proyecto={projectid} setOpenDialog={setOpenDialog} />
+            </Dialog>
+            </div>
+        </div>
+    );
+};
+
+const CrearAvance = ({ proyecto, setOpenDialog }) => {
+    const { userData } = useUser();
+    const { form, formData, updateFormData } = useFormData();
+
+    const [crearAvance, { loading }] = useMutation(CREAR_AVANCE, {
+        refetchQueries: [GET_AVANCES],
+    });
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        crearAvance({
+            variables: { ...formData, proyecto, creadoPor: userData._id },
+        })
+            .then(() => {
+                toast.success('avance creado con exito');
+                setOpenDialog(false);
+            })
+            .catch(() => {
+                toast.error('error creando el avance');
+            });
+    };
+    return (
+        <div className='p-4'>
+            <h1 className='Header-dialog'>Crear Nuevo Avance</h1>
+            <div className='H4-gray'>
+            <form ref={form} onChange={updateFormData} onSubmit={submitForm}>
+                <Input name='descripcion' label='Descripción' type='text' />
+                <Input name='fecha' label='Fecha' type='date' />
+                <ButtonLoading
+                    text='Crear Avance'
+                    loading={loading}
+                    disabled={Object.keys(formData).length === 0}
+                />
+            </form>
+            </div>
         </div>
     );
 };
