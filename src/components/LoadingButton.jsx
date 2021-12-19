c8d2e4366605046e093c2586845bb4024b035a1f
Jeferson <guevarajeferson@gmail.com>
2021-12-19 13:11:58 -0500

    Se agregan pruebas unitarias

--- src/components/LoadingButton.jsx
+++ src/components/LoadingButton.jsx
@@ -4,12 +4,14 @@ import ReactLoading from 'react-loading';
 const LoadingButton = ({ disabled, loading, text, bgColor = 'bg-green', onClick = () => { } }) => {
   return (
     <button
+      
       onClick={onClick}
       disabled={disabled}
       type='submit'
       className={`${bgColor} rounded-3xl shadow-md text-gray-dark font-bold font-Quicksand text-lg p-2.5 text-l rounded-xl hover:bg-gray-light shadow-md my-4 disabled:opacity-50 disabled:bg-gray`}
+      data-testid='button-loading'
     >
-      {loading ? <ReactLoading type='spin' height={30} width={30} /> : text}
+      {loading ? <ReactLoading data-testid='loading-in-button' type='spin' height={30} width={30} /> : text}
     </button>
   );
 };
